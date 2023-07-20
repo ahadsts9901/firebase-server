03b46af7ca450345b78bf8d2d70b93a292dec4d6
ahadsts9901 <ahadsts990@gmail.com>
2023-07-20 13:28:13 -0700

    remaining

--- router/server.mjs
+++ router/server.mjs
@@ -5,23 +5,13 @@ const __dirname = path.resolve();
 import apiv1Router from './apiv1/main.mjs'
 import apiv2Router from './apiv2/main.mjs'
 
-const app = express()
+const router = express.Router()
 
-app.use(express.json()); // body parser
+router.use(express.json()); // body parser
 
-app.use("/api/v1", apiv1Router)
-app.use("/api/v2", apiv2Router)
+router.use("/api/v1", apiv1Router)
+router.use("/api/v2", apiv2Router)
 
-// app.use("/static", express.static(path.join(__dirname, 'static')))
+router.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-const PORT = process.env.PORT || 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-})
+export default router
